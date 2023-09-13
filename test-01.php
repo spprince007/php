09157f4a6206ebf910a36c2786798b4aa514c72d
unknown <spprince007>
2023-09-13 22:38:14 +0600

    test commit

---
+++ test-01.php
@@ -0,0 +1,41 @@
+
+ Ans to the qus no 1 
+
+<?php
+$age = 28;
+// echo "I sm $age years old"
+?>
+
+ Ans to the qus no 2 
+
+<?php
+define("PI", 3.14159);
+?>
+
+ Ans to the qus no 3 
+
+<?php
+echo "Hello, welcome to the world of PHP programming!";
+?>
+
+Ans to the qus no 4 
+
+
+<?php
+ $result = 10 + 5  - 3 * 4;
+print $result;
+?>
+
+ Ans to the qus no 5
+
+<?php
+
+$marks = 70;
+
+if($marks>= 60 && $marks < 80){ // the qus say, the marks less then 80 wil be passed but equal or graterthen equal not condition thats why it will show he/she will be faild
+    print "You are passed";
+}else{
+    print "Your are Failed";
+}
+?>
+
